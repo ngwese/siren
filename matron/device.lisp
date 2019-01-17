6309a21e34e8d6c1a8926788ca34ca7666f3b5fc
Greg Wuller <greg@afofo.com>
2019-01-16 22:27:37 -0800

    symbols for types end with -t, expand package exports

--- matron/device.lisp
+++ matron/device.lisp
@@ -1,6 +1,6 @@
 (in-package :matron)
 
-(defctype ssize :int) ; MAINT: confirm against ssize_t
+(defctype ssize-t :int) ; MAINT: confirm against ssize_t
 
 ;;;
 ;;; device common
@@ -13,20 +13,20 @@
   :dev-type-count
   :dev-type-invalid)
 
-(defctype dev-start :pointer)
-(defctype dev-deinit :pointer)
+(defctype dev-start-t :pointer)
+(defctype dev-deinit-t :pointer)
 
-(defctype pthread-t :uint32) ; FIXME: this needs to be confirmed or dev-common is possibly wrong
+(defctype pthread-t :uint32) ; FIXME: this needs to be confirmed or dev-common-t is possibly wrong
 
-(defcstruct dev-common
+(defcstruct dev-common-t
   (type device-t)
   (id :uint32)
   (tid pthread-t)
   (path :string)
   (serial :string)
   (name :string)
-  (start dev-start)
-  (deinit dev-deinit))
+  (start dev-start-t)
+  (deinit dev-deinit-t))
 
 ;;;
 ;;; device monome
@@ -42,49 +42,49 @@
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (defconstant +monome-buffer-size+ 256))
 
-(defcstruct dev-monome
-  (dev (:struct dev-common))
+(defcstruct dev-monome-t
+  (dev (:struct dev-common-t))
   (type device-monome-type-t)
   (m (:pointer monome-t))
   (data :uint8 :count 256) ; led data by quad
   (dirty :bool :count 4))
 
-(defctype dev-monome-ptr (:pointer (:struct dev-monome)))
+(defctype dev-monome-ptr-t (:pointer (:struct dev-monome-t)))
 
 (defcfun "dev_monome_grid_set_led" :void
-  (md dev-monome-ptr)
+  (md dev-monome-ptr-t)
   (x :uint8)
   (y :uint8)
   (val :uint8))
 
 (defcfun "dev_monome_arc_set_led" :void
-  (md dev-monome-ptr)
+  (md dev-monome-ptr-t)
   (x :uint8)
   (y :uint8)
   (val :uint8))
 
 (defcfun "dev_monome_all_led" :void
-  (md dev-monome-ptr)
+  (md dev-monome-ptr-t)
   (val :uint8))
 
 (defcfun "dev_monome_set_quad" :void
-  (md dev-monome-ptr)
+  (md dev-monome-ptr-t)
   (quad :uint8)
   (data (:pointer :uint8)))
 
-(defcfun "dev_monome_refresh" :void (md dev-monome-ptr))
-(defcfun "dev_monome_grid_rows" :int (md dev-monome-ptr))
-(defcfun "dev_monome_grid_cols" :int (md dev-monome-ptr))
+(defcfun "dev_monome_refresh" :void (md dev-monome-ptr-t))
+(defcfun "dev_monome_grid_rows" :int (md dev-monome-ptr-t))
+(defcfun "dev_monome_grid_cols" :int (md dev-monome-ptr-t))
 
 (defcfun "dev_monome_set_rotation" :void
-  (md dev-monome-ptr)
+  (md dev-monome-ptr-t)
   (val :uint8))
 
-(defctype %monome-self-ptr (:pointer :void))
+(defctype %monome-self-ptr-t (:pointer :void))
 
-(defcfun "dev_monome_init" :int (self %monome-self-ptr))
-(defcfun "dev_monome_deinit" :void (self %monome-self-ptr))
-(defcfun "dev_monome_start" :void (self %monome-self-ptr))
+(defcfun "dev_monome_init" :int (self %monome-self-ptr-t))
+(defcfun "dev_monome_deinit" :void (self %monome-self-ptr-t))
+(defcfun "dev_monome_start" :void (self %monome-self-ptr-t))
 
 ;;;
 ;;; device hid
@@ -96,8 +96,8 @@
 
 (defctype libevdev :void)
 
-(defcstruct dev-hid
-  (base (:struct dev-common))
+(defcstruct dev-hid-t
+  (base (:struct dev-common-t))
   (dev (:pointer libevdev))
   (vid dev-vid-t)
   (pid dev-pid-t)
@@ -106,11 +106,11 @@
   (num-codes (:pointer :int))
   (codes (:pointer (:pointer dev-code-t))))
 
-(defctype %hid-self-ptr (:pointer :void))
+(defctype %hid-self-ptr-t (:pointer :void))
 
-(defcfun "dev_hid_init" :int (self %hid-self-ptr) (print :bool))
-(defcfun "dev_hid_deinit" :void (self %hid-self-ptr))
-(defcfun "dev_hid_start" (:pointer :void) (self %hid-self-ptr))
+(defcfun "dev_hid_init" :int (self %hid-self-ptr-t) (print :bool))
+(defcfun "dev_hid_deinit" :void (self %hid-self-ptr-t))
+(defcfun "dev_hid_start" (:pointer :void) (self %hid-self-ptr-t))
 
 ;;;
 ;;; device midi
@@ -118,18 +118,18 @@
 
 (defctype snd-rawmidi-t :void)
 
-(defcstruct dev-midi
-  (dev (:struct dev-common))
+(defcstruct dev-midi-t
+  (dev (:struct dev-common-t))
   (handle-in (:pointer snd-rawmidi-t))
   (handle-out (:pointer snd-rawmidi-t)))
 
-(defctype %midi-self-ptr (:pointer :void))
+(defctype %midi-self-ptr-t (:pointer :void))
 
-(defcfun "dev_midi_init" :int (self %midi-self-ptr))
-(defcfun "dev_midi_deinit" :void (self %midi-self-ptr))
-(defcfun "dev_midi_start" (:pointer :void) (self %midi-self-ptr))
-(defcfun "dev_midi_send" ssize
-  (self %midi-self-ptr)
+(defcfun "dev_midi_init" :int (self %midi-self-ptr-t))
+(defcfun "dev_midi_deinit" :void (self %midi-self-ptr-t))
+(defcfun "dev_midi_start" (:pointer :void) (self %midi-self-ptr-t))
+(defcfun "dev_midi_send" ssize-t
+  (self %midi-self-ptr-t)
   (data (:pointer :uint8))
   (n size))
 
@@ -137,23 +137,23 @@
 ;;; device
 ;;;
 
-(defcunion dev
-  (base (:struct dev-common))
-  (monome (:struct dev-monome))
-  (hid (:struct dev-hid))
-  (midi (:struct dev-midi)))
+(defcunion dev-t
+  (base (:struct dev-common-t))
+  (monome (:struct dev-monome-t))
+  (hid (:struct dev-hid-t))
+  (midi (:struct dev-midi-t)))
 
-(defctype dev-ptr (:pointer (:union dev)))
+(defctype dev-ptr-t (:pointer (:union dev-t)))
 
 (defcfun "devices_init" :void)
-(defcfun "dev_new" dev-ptr
+(defcfun "dev_new" dev-ptr-t
   (type device-t)
   (path :string)
   (name :string))
-(defcfun "dev_delete" :void (d dev-ptr))
-(defcfun "dev_id" :int (d dev-ptr))
-(defcfun "dev_serial" :string (d dev-ptr))
-(defcfun "dev_name" :string (d dev-ptr))
+(defcfun "dev_delete" :void (d dev-ptr-t))
+(defcfun "dev_id" :int (d dev-ptr-t))
+(defcfun "dev_serial" :string (d dev-ptr-t))
+(defcfun "dev_name" :string (d dev-ptr-t))
 
 ;;;
 ;;; device list
--- matron/events.lisp
+++ matron/events.lisp
@@ -1,6 +1,6 @@
 (in-package :matron)
 
-(defctype size :unsigned-int) ; MAINT: confirm against size_t
+(defctype size-t :unsigned-int) ; MAINT: confirm against size_t
 
 ;;;
 ;;; callback types
@@ -16,22 +16,22 @@
   :poll-type-value
   :poll-type-data)
 
-(defcstruct engine-command
+(defcstruct engine-command-t
   (name :string)
   (format :string))
 
-(defcstruct engine-poll
+(defcstruct engine-poll-t
   (name :string)
   (type poll-type-t))
 
-(defcstruct engine-param
+(defcstruct engine-param-t
   (name :string)
   (bus-idx :int))
 
 ;;;
 ;;; osc types
 ;;;
-(defctype osc-message :pointer)
+(defctype osc-message-t :pointer)
 
 ;;;
 ;;; event structures
@@ -77,156 +77,156 @@
 	 (common (:pointer (:struct event-common)))
 	 ,@body))
 
-(defcstruct event-common
+(defcstruct event-common-t
   (type :uint32)
   (free free-event-data-t))
 
-(%defevent event-exec-code-line
+(%defevent event-exec-code-line-t
   (line :string))
 
-(%defevent event-monome-add
+(%defevent event-monome-add-t
   (dev :pointer))
 
-(%defevent event-monome-remove
+(%defevent event-monome-remove-t
   (id :uint32))
 
-(%defevent event-grid-key
+(%defevent event-grid-key-t
   (id :uint8)
   (x :uint8)
   (y :uint8)
   (state :uint8))
 
-(%defevent event-arc-encoder-delta
+(%defevent event-arc-encoder-delta-t
   (id :uint8)
   (number :uint8)
   (delta :int8))
 
-(%defevent event-arc-encoder-key
+(%defevent event-arc-encoder-key-t
   (id :uint8)
   (number :uint8)
   (state :int8))
 
-(%defevent event-hid-add
+(%defevent event-hid-add-t
   (dev :pointer))
 
-(%defevent event-hid-remove
+(%defevent event-hid-remove-t
   (id :uint32))
 
-(%defevent event-hid-event
+(%defevent event-hid-event-t
   (id :uint8)
   (type :uint8)
   (code :uint16)
   (value :int32))
 
-(%defevent event-midi-add
+(%defevent event-midi-add-t
   (dev :pointer))
 
-(%defevent event-midi-remove
+(%defevent event-midi-remove-t
   (id :uint32))
 
-(%defevent event-midi-event
+(%defevent event-midi-event-t
   (id :uint32)
   (data :uint8 :count 3)
-  (nbytes size))
+  (nbytes size-t))
 
-(%defevent event-osc
+(%defevent event-osc-t
   (path :string)
   (from-host :string)
   (from-port :string)
   (msg osc-message))
 
-(%defevent event-metro
+(%defevent event-metro-t
   (id :uint32)
   (stage :uint32))
 
-(%defevent event-key
+(%defevent event-key-t
   (id :uint8)
   (val :uint8))
 
-(%defevent event-battery
+(%defevent event-battery-t
   (percent :uint8)
   (current :int16))
 
-(%defevent event-power
+(%defevent event-power-t
   (present :uint8))
 
-(%defevent event-stat
+(%defevent event-stat-t
   (disk :uint16)
   (temp :uint8)
   (cpu :uint8))
 
-(%defevent event-enc
+(%defevent event-enc-t
   (n :uint8)
   (delta :int8))
 
-(%defevent event-poll-value
+(%defevent event-poll-value-t
   (idx :uint32)
   (value :float))
 
-(%defevent event-poll-io-levels
+(%defevent event-poll-io-levels-t
   (idx :uint32)
   (value (:union quad-levels-t)))
 
-(%defevent event-poll-data
+(%defevent event-poll-data-t
   (idx :uint32)
   (size :uint32)
   (data (:pointer :uint8)))
 
-(%defevent event-poll-wave
+(%defevent event-poll-wave-t
   (idx :uint32)
   (data (:pointer :uint8)))
 
-(%defevent event-startup-ready-ok)
-(%defevent event-startup-ready-timeout)
+(%defevent event-startup-ready-ok-t)
+(%defevent event-startup-ready-timeout-t)
 
-(defcunion event-data
+(defcunion event-data-t
   (type :uint32)
-  (common (:struct event-common))
-  (exec-code-line (:struct event-exec-code-line))
-  (monome-add (:struct event-monome-add))
-  (monome-remove (:struct event-monome-remove))
-  (grid-key (:struct event-grid-key))
-  (arc-encoder-delta (:struct event-arc-encoder-delta))
-  (arc-encoder-key (:struct event-arc-encoder-key))
-  (hid-add (:struct event-hid-add))
-  (hid-remove (:struct event-hid-remove))
-  (hid-event (:struct event-hid-event))
-  (midi-add (:struct event-midi-add))
-  (midi-remove (:struct event-midi-remove))
-  (midi-event (:struct event-midi-event))
-  (osc-event (:struct event-osc))
-  (key (:struct event-key))
-  (enc (:struct event-enc))
-  (battery (:struct event-battery))
-  (power (:struct event-power))
-  (stat (:struct event-stat))
-  (metro (:struct event-metro))
-  (poll-value (:struct event-poll-value))
-  (poll-data (:struct event-poll-data))
-  (poll-io-levels (:struct event-poll-io-levels))
-  (poll-wave (:struct event-poll-wave))
-  (startup-ready-ok (:struct event-startup-ready-ok))
-  (startup-ready-timeout (:struct event-startup-ready-timeout)))
+  (common (:struct event-common-t))
+  (exec-code-line (:struct event-exec-code-line-t))
+  (monome-add (:struct event-monome-add-t))
+  (monome-remove (:struct event-monome-remove-t))
+  (grid-key (:struct event-grid-key-t))
+  (arc-encoder-delta (:struct event-arc-encoder-delta-t))
+  (arc-encoder-key (:struct event-arc-encoder-key-t))
+  (hid-add (:struct event-hid-add-t))
+  (hid-remove (:struct event-hid-remove-t))
+  (hid-event (:struct event-hid-event-t))
+  (midi-add (:struct event-midi-add-t))
+  (midi-remove (:struct event-midi-remove-t))
+  (midi-event (:struct event-midi-event-t))
+  (osc-event (:struct event-osc-t))
+  (key (:struct event-key-t))
+  (enc (:struct event-enc-t))
+  (battery (:struct event-battery-t))
+  (power (:struct event-power-t))
+  (stat (:struct event-stat-t))
+  (metro (:struct event-metro-t))
+  (poll-value (:struct event-poll-value-t))
+  (poll-data (:struct event-poll-data-t))
+  (poll-io-levels (:struct event-poll-io-levels-t))
+  (poll-wave (:struct event-poll-wave-t))
+  (startup-ready-ok (:struct event-startup-ready-ok-t))
+  (startup-ready-timeout (:struct event-startup-ready-timeout-t)))
 
 ;;;
 ;;; event api
 ;;;
 
-(defctype event-data-ptr (:pointer (:union event-data)))
+(defctype event-data-ptr-t (:pointer (:union event-data-t)))
 
 ;; signals the event_loop to stop
-(defcvar ("quit" *quit*) :bool)
+(defcvar ("quit" *event-loop-quit*) :bool)
 
 (defconstant +event-wave-display-bytes+ 128)
 
 (defcfun "events_init" :void)
 (defcfun "event_set_handler" :void (h event-handler-t))
 (defcfun "event_loop" :void)
-(defcfun "event_data_new" event-data-ptr
+(defcfun "event_data_new" event-data-ptr-t
   (evcode event-t)
   (cb free-event-data-t))
 (defcfun "event_data_free" :void
-  (ev event-data-ptr))
+  (ev event-data-ptr-t))
 (defcfun "event_post" :void
-  (ev event-data-ptr))
+  (ev event-data-ptr-t))
 
