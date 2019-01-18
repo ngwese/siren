1b191edcb697f6f0f47987c9fd6a3b3589905d3e
Greg Wuller <greg@afofo.com>
2019-01-17 21:32:12 -0800

    display key, enc on screen

--- siren/test-controls.lisp
+++ siren/test-controls.lisp
@@ -1,12 +1,30 @@
 (in-package :siren-user)
 
-;; (cffi:defcallback demo-handler :void ((ev event-data-ptr-t))
-;;   (cffi:with-foreign-slots ((type) ev event-data-ptr-t)
-;; 	(format t "ev type: ~D~%" type)))
-
 (cffi:defcallback demo-handler :void ((ev event-data-ptr-t))
-  (let ((type (cffi:foreign-slot-value ev '(:union event-data-t) 'type)))
-	(format t "ev type: ~D~%" type)
+  (let* ((type-num (cffi:foreign-slot-value ev '(:union event-data-t) 'type))
+		 (type (cffi:foreign-enum-keyword 'event-t type-num)))
+;	(format t "ev type: ~A (~D) // " type type-num) 
+	
+	(case type
+	  (:event-key
+	   (cffi:with-foreign-slots ((matron::id matron::val) ev (:struct event-key-t))
+	  	 (screen-clear)
+		 (screen-level 15)
+		 (screen-move 10d0 10d0)
+		 (screen-text (format nil "id: ~D val: ~D" matron::id matron::val))
+		 (screen-update)
+		 ))
+	  (:event-enc
+	   (cffi:with-foreign-slots ((matron::n matron::delta) ev (:struct event-enc-t))
+	  	 (screen-clear)
+		 (screen-level 15)
+		 (screen-move 10d0 20d0)
+		 (screen-text (format nil "n: ~D delta: ~D" matron::n matron::delta))
+		 (screen-update)))
+	  (otherwise
+	   (format t "=> no match? ~A ~A~%" (type-of type) (type-of :event-type-key))
+										;(setq *event-loop-quit* t)
+	   ))
 	(event-data-free ev)))
 
 (defun test-screen ()
